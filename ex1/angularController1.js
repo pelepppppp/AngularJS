2379d629a6b720978b5891d827281ba096436153
Student Student <student@ceaccs.net>
2019-06-13 13:56:18 +0800

    Files added

---
+++ ex1/angularController1.js
@@ -0,0 +1,12 @@
+application.controller("myController", function($scope){
+    $scope.Submit = function(){
+        var name = document.getElementById('name').value + ". ";
+        var birthday = document.getElementById('month').value + document.getElementById('day').value + ", "+ document.getElementById('year').value + "!";
+        var hi = "Hello";
+        var bd = "Your birthday is"
+        $scope.greet = hi;
+        $scope.yourName = name;
+        $scope.words = bd;
+        $scope.birthday = birthday ;
+    }
+});
