f6d45719cf714e15b425b8d2327e9e3bf29a2ea4
Student Student <student@ceaccs.net>
2019-06-14 15:56:04 +0800

    HUMANA

--- Project/app.js
+++ Project/app.js
@@ -1,61 +1,70 @@
 angular.module("myApp", ["ngRoute"])
-.config(function($routeProvider){
-    $routeProvider
-    .when("/",{
-        controller: "indexController",
-        controllerAs: "index",
-        templateUrl: "views/index.html"
-    })
-    .when("/aboutUs",{
-        controller: "aboutUsController",
-        controllerAs: "aboutUs",
-        templateUrl: "views/aboutUs.html"
-    })
-    .when("/faq",{
-        controller: "faqController",
-        controllerAs: "faq",
-        templateUrl: "views/faq.html"
-    })
-    .otherwise({redirectTo: "/"})
-})
+	.config(function ($routeProvider) {
+		$routeProvider
+			.when("/", {
+				controller: "indexController",
+				controllerAs: "index",
+				templateUrl: "views/index.html"
+			})
+			.when("/aboutUs", {
+				controller: "aboutUsController",
+				controllerAs: "aboutUs",
+				templateUrl: "views/aboutUs.html"
+			})
+			.when("/faq", {
+				controller: "faqController",
+				controllerAs: "faq",
+				templateUrl: "views/faq.html"
+			})
+			.otherwise({
+				redirectTo: "/"
+			})
+	})
 
-.controller("indexController", function($scope){
-    $scope.people = [];
+	.controller("indexController", function ($scope) {
+		$scope.people = [];
 
-    //  to add a person
-    $scope.enroll = function(){
-        $scope.people.push({
-            Name: $scope.name,
-            Address: $scope.address,
-            Age: $scope.age
-        });
+		//  to add a person
+		$scope.enroll = function () {
+			$scope.people.push({
+				Name: $scope.name,
+				Address: $scope.address,
+				Age: $scope.age
+			});
+			$scope.clearFields();
+		};
 
-        $scope.name = "";
-        $scope.address = "";
-        $scope.age = "";
-    };
-
-    //  to remove a person
-    $scope.remove = function(){
-        $scope.people.splice($scope.toRemove, 1);
-        $scope.toRemove = null;
-    };
-    
-    //  to update info
-    $scope.update = function(name, address, age){
-        $scope.name = angular.copy(name);
-    };
-
-    //  function for updatePerson()
-    $scope.updatePerson = function(){
-
-    };
+		//  to remove a person
+		$scope.remove = function (index) {
+			$scope.people.splice(index, 1);
+		}
 
 
-})
-.controller("aboutUsController", function($scope){
-    $scope.aboutUsMessage = "I am 22 years old."
-})
-.controller("faqController", function($scope){
-    $scope.faqMessage = "Harry Potter!"
-})
+		//  to update info
+		$scope.update = function (index, name, address, age) {
+			$scope.name = angular.copy(name);
+			$scope.address = angular.copy(address);
+			$scope.age = angular.copy(age);
+			$scope.selectedIndex = index;
+		};
+
+		//  function for updatePerson()
+		$scope.updatePerson = function () {
+			$scope.people[$scope.selectedIndex].Name = $scope.name;
+			$scope.people[$scope.selectedIndex].Address = $scope.address;
+			$scope.people[$scope.selectedIndex].Age = $scope.age;
+			$scope.clearFields();
+
+		};
+		$scope.clearFields = function () {
+			$scope.name = "";
+			$scope.address = "";
+			$scope.age = "";
+		}
+	})
+	.controller("aboutUsController", function ($scope) {
+		$scope.aboutUsMessage = "I am 22 years old."
+	})
+	.controller("faqController", function ($scope) {
+		$scope.faqMessage = "Harry Potter!"
+	})
